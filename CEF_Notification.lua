6dd249c2a1db2627dad645c1da56fcccf6efce15
Dipcooler <99092423+Dipcooler@users.noreply.github.com>
2023-09-03 14:11:42 +0300

    Update CEF_Notification.lua

--- CEF_Notification.lua
+++ CEF_Notification.lua
@@ -1,7 +1,3 @@
--- Автор этой библиотеки: MTG MODS (https://www.blast.hk/members/456827/)
--- Оптимизация и фикс говнокода: by Chapo (https://www.blast.hk/members/112329/)
--- Инструкция по использованию этой библиотеки: https://www.blast.hk/threads/187327/
-
 return function(type, title, text, time)
     local str = ('window.executeEvent(\'event.notify.initialize\', \'["%s", "%s", "%s", "%s"]\');'):format(type, title, text, time)
     local bs = raknetNewBitStream()
